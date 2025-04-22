67880246b7dd8ba498dc2fe828efa473d8a1bc47
CarlosCode368 <carlossarabia_r@hotmail.com>
2025-04-22 13:44:44 -0700

    Initial commit

---
+++ src/D_PayrollCalculator.java
@@ -0,0 +1,24 @@
+
+import java.io.*;
+public class D_PayrollCalculator {
+    public static void main(String[] args) {
+        try
+        {
+// create a FileReader object connected to the File
+            FileReader fileReader = new FileReader("employees.txt");
+// create a BufferedReader to manage input stream
+            BufferedReader bufReader = new BufferedReader(fileReader);
+            String input;
+// read until there is no more data
+            while((input = bufReader.readLine()) != null) {
+                System.out.println(input);
+            }
+// close the stream and release the resources
+            bufReader.close();
+        }
+        catch(IOException e) {
+// display stack trace if there was an error
+            e.printStackTrace();
+        }
+    }
+}
